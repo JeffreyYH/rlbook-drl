508b1b32d10375e42983f2575142f5b1b9923aa1
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2021-09-04 23:42:03 -0400

    update

--- ch02-Multi-armed-Bandits/10armedTestBed.py
+++ ch02-Multi-armed-Bandits/10armedTestBed.py
@@ -5,7 +5,7 @@ import matplotlib.pylab as plt
 class TenArmedTestbed:
     def __init__(self):
         self.T = 1000
-        self.num_run = 200
+        self.num_run = 100
 
     def greedy(self, q_a_vec):
         q_a_vec = np.array(q_a_vec)
@@ -13,17 +13,18 @@ class TenArmedTestbed:
         return a
 
     def ε_greedy(self, q_a_vec, ε):
-        if (np.random.uniform(0, 1) <= ε):
+        p = np.random.uniform(0, 1)
+        if (p <= ε):
             a = np.random.choice(range(10))
         else:
             a = self.greedy(q_a_vec)
         return a
 
     def tenArmed_Testbed(self):
-        R_ave_vec = []
-        for t in range(self.T):
-            R_all = 0
-            for run_idx in range(self.num_run):
+        r_all_vec = []
+        for run_idx in range(self.num_run):
+            r_vec = []
+            for t in range(self.T):
                 R = 0
                 q_a_vec = []
                 for i in range (10):  
@@ -36,12 +37,12 @@ class TenArmedTestbed:
                 # sample reward
                 q_a_optimal = q_a_vec[a]
                 r = np.random.normal(q_a_optimal, 1.0, num_sample)
-                R += r[0]
-                R_all += R
-            R_ave = R_all/self.num_run
-            R_ave_vec.append(R_ave)
+                r_vec.append(r)
+            r_all_vec.append(r_vec)
         # plot the results
-        plt.plot(R_ave_vec)
+        r_all_vec_np = np.array(r_all_vec)
+        r_all_vec_ave = np.sum(r_all_vec_np, axis=0)/self.num_run
+        plt.plot(r_all_vec_ave)
         plt.show()
 
 
