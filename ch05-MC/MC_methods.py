49b2f9cb3989c764d8854bfaea8bb1de7a99d3f5
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2022-03-09 22:02:40 -0500

    update

--- ch05-MC/MC_methods.py
+++ ch05-MC/MC_methods.py
@@ -10,22 +10,30 @@ from collections import namedtuple
 from blackjack_example import plot_value_function, handcrafted_episode
 
 
-class Tabular_MC_Agent:
+class Tabular_MC:
     def __init__(self, args):
         self.env = args.env
-        self.num_episodes=5000
+        self.num_episodes=10000
         self.max_steps=1000
-        self.epislon=0.01
+        self.epislon=0.1
         self.gamma = 1.0
 
-    def generate_episode(self, policy):
+    def generate_episode(self, policy, use_ES):
         """ each element in the episode is a [state, action, reward] tuple
-        for blackjack env: each state consists of : score, dealer_score, usable_ace """
+        inputs: 
+            policy: pi(a|s)
+            use_ES: use exploring starts or not
+        notes:
+            for blackjack env: each state consists of : score, dealer_score, usable_ace 
+        """
         init_state = self.env.reset()
         curr_state = init_state
         episode = []
         for step in range(self.max_steps):
-            action = policy[curr_state]
+            if step == 0 and use_ES:
+                action = np.random.choice(self.env.nA)
+            else:
+                action = policy[curr_state]
             next_state, reward, done, _ = self.env.step(action)
             episode.append((curr_state, action, reward))
             if done:
@@ -33,7 +41,7 @@ class Tabular_MC_Agent:
             else:
                 curr_state = next_state
         return episode
-
+    
 
     def first_visit_MC_prediction(self):
         # returns for state s (could be multiple returns in one state), value function
@@ -70,7 +78,6 @@ class Tabular_MC_Agent:
 
     def MC_ES(self):
         """ Monte Carlo ES (Exploring Starts) """
-        gamma = 1.0
         Returns = {}
         for s in range(self.env.nS):
             Returns[s] = {}
@@ -83,28 +90,39 @@ class Tabular_MC_Agent:
             policy_MCES[i] = np.random.choice(self.env.nA)
 
         for i_episode in tqdm(range(self.num_episodes)):
-            episode = self.generate_episode(policy_MCES)
+            # generate s_0, a_0 to fulfil exploring start assumption
+            episode = self.generate_episode(policy_MCES, True)
             G = 0
             T = len(episode)
+            visited_SA = []
             for t in reversed(range(T)):
                 s_t = episode[t][0]
                 a_t = episode[t][1]
                 r_t = episode[t][2]
-                G = gamma * G + r_t
-                past_states = np.array(episode, dtype=int)[0:t, 0].tolist()
-                past_actions = np.array(episode, dtype=int)[0:t, 1].tolist()
-                if s_t in past_states and a_t in past_actions:
-                    Returns[s_t][a_t].append(G)
-                    Q[s_t][a_t] = np.mean(np.array(Returns[s_t][a_t]))
-                    policy_MCES[s_t] = np.argmax(Q[s_t, :])
-        
+                G = self.gamma * G + r_t
+                if [s_t, a_t] in visited_SA:
+                    continue
+                Returns[s_t][a_t].append(G)
+                Q[s_t][a_t] = np.mean(np.array(Returns[s_t][a_t]))
+                policy_MCES[s_t] = np.argmax(Q[s_t, :])
+                visited_SA.append([s_t, a_t])
         print(Q)
         return policy_MCES
+
+    
+    def epsilon_greedy_policy(self, q_values, ε=0.05):
+        """ Creating epsilon greedy probabilities to sample from """
+        """ when ε=0, it is greedy """
+        p = np.random.uniform(0, 1)
+        if (p < ε):
+            a = np.random.choice(len(q_values))
+        else:
+            a = np.argmax(np.array(q_values))
+        return a
 
 
     def OnPolicy_first_visit_MC_control(self):
         """ On-policy first-visit MC control """
-        gamma = 1.0
         Returns = {}
         for s in range(self.env.nS):
             Returns[s] = {}
@@ -116,21 +134,33 @@ class Tabular_MC_Agent:
             policy_FVMCC[i] = np.random.choice(self.env.nA)
 
         for i_episode in tqdm(range(self.num_episodes)):
-            episode = self.generate_episode(policy_FVMCC)
+            episode = self.generate_episode(policy_FVMCC, False)
             G = 0
             T = len(episode)
+            visited_SA = []
             for t in reversed(range(T)):
                 s_t = episode[t][0]
                 a_t = episode[t][1]
                 r_t = episode[t][2]
-                G = gamma * G + r_t
-                past_states = np.array(episode, dtype=int)[0:t, 0].tolist()
-                past_actions = np.array(episode, dtype=int)[0:t, 1].tolist()
-                if s_t in past_states and a_t in past_actions:
-                    Returns[s_t][a_t].append(G)
-                    Q[s_t][a_t] = np.mean(np.array(Returns[s_t][a_t]))
-                    policy_FVMCC[s_t] = np.argmax(Q[s_t, :])
+                G = self.gamma * G + r_t
+                # SA_pairs = np.array(episode, dtype=int)[0:t, 0:2].tolist()
+                if [s_t, a_t] in visited_SA:
+                    continue
+                # if [s_t, a_t] in SA_pairs:
+                Returns[s_t][a_t].append(G)
+                Q[s_t][a_t] = np.mean(np.array(Returns[s_t][a_t]))
+                policy_FVMCC[s_t] = self.epsilon_greedy_policy(Q[s_t, :], self.epislon)
+                visited_SA.append([s_t, a_t])
         print(Q)
+        # print(policy_FVMCC)
+
+
+    def offPolicy_MC_prediction(self):
+        pass
+    
+
+    def offPolicy_MC_control(self):
+        pass
 
 
 def parse_arguments():
@@ -145,8 +175,9 @@ if __name__ == "__main__":
     args = parse_arguments()
     args.env = gym.make(args.env_name)
 
-    MC_agent = Tabular_MC_Agent(args)
+    MC_agent = Tabular_MC(args)
 
+    MC_agent.MC_ES()
     MC_agent.OnPolicy_first_visit_MC_control()
 
     if args.env_name == "Blackjack-v1":
