689e8885e60d2f36f43eb5ca2e9266e3c9b0bf84
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2022-03-17 17:59:54 -0400

    update

--- ch06_TD/nStep_TD.py
+++ ch06_TD/nStep_TD.py
@@ -15,10 +15,46 @@ class Tabular_nStepTD:
         self.num_episodes=10000
         self.gamma = 1.0
         self.alpha = 0.05
+        self.n = 5
         self.tabularUtils = TabularUtils(self.env)
     
-    def nStepTD_prediction(self):
-        pass
+    def nStepTD_prediction(self, policy, n):
+        V_est = np.zeros(self.env.nS)
+        for epi in range(self.num_episode):
+            done = False
+            t = 0
+            s = self.env.reset()
+            T = float("inf")
+            S = []
+            S.append(s) # append intial state
+            R = []
+            R.append(0)
+            while True:
+                if t < T:
+                    a = policy[s]
+                    s_next, r, done, _ = self.env.step(a)
+                    S.append(s_next)
+                    R.append(r)
+                    if done:
+                        T = t+1
+                # the time step whose state’s estimate is being updated
+                τ = t - n + 1
+                if τ >= 0:
+                    # compute return G
+                    G = 0
+                    for i in range(τ+1, min(τ+n, T)+1):
+                        G += (self.gamma**(i-τ-1)) * R[i]
+                    if τ + n < T:
+                        G = G + (self.gamma**n) * V_est[S[τ+n]]
+                    V_est[S[τ]] = V_est[S[τ]] + self.alpha * (G - V_est[S[τ]])
 
+                # move to next time step
+                s = s_next
+                t += 1
+
+                if τ == T-1:
+                    break
+
+        return V_est
 
     
